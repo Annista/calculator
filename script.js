68629c6a6f910619aa74d579b783746029c7e1ec
Annista <stephanie11foster@gmail.com>
2021-10-12 01:04:37 -0500

    Added a function that find and carry out division and multiplcation in the expression entered, and another that would carry out addition and subtraction afterwards; Added an error message that would be displayed if the expression entered is invalid

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+//Each of the following four functions perform a basic arithmetic operation for the calculator
+
 function add(...arr){
     return arr.reduce((prev, curr)=> prev+curr);
 
@@ -9,27 +11,33 @@ function subtract(...arr){
 
 function multiply(...arr){
     return arr.reduce((prev, curr)=> prev*curr);
+
 }
 
 function divide(...arr){
     const result = arr.reduce((prev, curr)=> prev/curr);
-
-    if(result===Infinity){
-        return 'MATH Error: Number cannot be divided by zero';
-    }else{
-        if (isNaN(result)){
-            return 'Inconclusive';
+        
+        if(result===Infinity){                                     //  the result would be Infinity if 
+            throw 'MATH Error: Number cannot be divided by zero';  // a number(execpt 0) is divided by 0
         }else{
-            return result;
-      }
-  }
+            if (isNaN(result)){                         //  the result would be NaN 
+                throw 'MATH Error: Inconclusive';      //  if 0 is divided by 0
+            }
+       }
+       return result;
+
+       
+       
 }
+
+// This function determines which of the arithmetic operations should be performed on the expression 
+// given to it. After, it will return the answer to the expression.
 
 function operate(num1, operator, num2){
 
     switch(operator){
         case '+':
-            return add(num1, num2);
+            return add(parseFloat(num1), parseFloat(num2));
             break;
 
         case '-':
@@ -42,6 +50,7 @@ function operate(num1, operator, num2){
             
         case '/':
             return  divide(num1, num2);
+            
             break;
             
         default:
@@ -51,31 +60,33 @@ function operate(num1, operator, num2){
 
 }
 
-/*console.log(subtract((add(9, (multiply((divide(18, 12)), 81)))), 7));
-console.log(divide(9, 0));
-console.log(add(5, 6));
-
-console.log(operate(4, '+', 2));
-console.log(operate(3, '/', 6));
-console.log(operate(9, '*', 9));
-console.log(operate(15, '/', 0));*/
-
 
 const operatorButtons= document.querySelectorAll('.oper-button');
 const numButtons= document.querySelectorAll('.num-button');
 const calcDisplay= document.querySelector('#calculator-display');
 const clearButton= document.querySelector('#clear-button');
 const equalButton= document.querySelector('#equal-button');
+const divideButton= document.querySelector('#divide-button');
+const multiplyButton= document.querySelector('#multiply-button');
 
 let displayValue="";
+let displayValArray=[];
+const divSymbol=divideButton.textContent;
+const multSymbol=multiplyButton.textContent;
+
+
+//   For the following two forEach methods:
+//   When any of the calculator buttons (numbers and arithmeric operations) 
+//   are clicked, it will be shown on the display
 
 operatorButtons.forEach((btn)=>{
     btn.addEventListener('click', ()=>{
-        calcDisplay.value+=btn.textContent;
+        calcDisplay.value+=btn.textContent;    
 
-    });
+    });                                     
 
-});
+
+});         
 
 numButtons.forEach((btn)=>{
     btn.addEventListener('click', ()=>{
@@ -85,13 +96,77 @@ numButtons.forEach((btn)=>{
 
 });
 
+
+//To clear the display of the calculator
+
 clearButton.addEventListener('click', ()=>{
     calcDisplay.value="";
 
 });
 
+
+function calcDivideMultiply(arr){ //preferred function
+    let symbol='';
+
+    const index= arr.findIndex(item=> (item===divSymbol || item===multSymbol) );
+
+    if (index===-1) { return arr; }
+    
+    const dispSymbol= arr.find(item=> (item===divSymbol || item===multSymbol));
+
+    (dispSymbol===divSymbol) ?  symbol= '/' : symbol= '*';
+    const result=operate(arr[index-1], symbol, arr[index+1]);
+
+    const newArray=[
+                    ...arr.slice(0, index-1),
+                    result,
+                    ...arr.slice([index+2])
+                ];
+    return anotherdivMultCalc(newArray);
+    
+}
+
+
+function calcAddSubtract(arr){ //preferred function
+    const index= arr.findIndex( item => (item==='-' || item==='+'));
+
+    if(index===-1){
+        return arr[0];
+    }
+
+    const symbol=arr.find( item=> (item==='-' || item==='+'));
+
+    result=operate(arr[index-1], symbol, arr[index+1]);
+    const newArray=[
+        ...arr.slice(0, index-1),
+        result,
+        ...arr.slice([index+2])
+     ];
+     return anotherAddSubtCalc(newArray);
+
+
+}
+
+
 equalButton.addEventListener('click', ()=>{
     displayValue= calcDisplay.value;
     console.log(displayValue);
+    displayValArray= displayValue.split(/(\D)/);
+    console.log(displayValArray);
+    /*console.log(divSymbol);
+    console.log(multSymbol);*/
+
+
+    try{
+    const arrayNext= anotherdivMultCalc(displayValArray);
+    //console.log(arrayNext);
+    console.log(anotherAddSubtCalc(arrayNext));
+
+    }catch(error){
+        console.log(error);
+
+
+    }  
 
 });
+
