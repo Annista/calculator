04e0a7b67eea0f36680d0bba74199b8ba55e28ce
Annista <stephanie11foster@gmail.com>
2021-11-01 09:27:00 -0500

    Changed backspace button functionality so that it removes last item in the calculator expression

--- new-script.js
+++ new-script.js
@@ -372,16 +372,15 @@ backspaceButton.addEventListener('click', ()=>{
     let exprArrayInd=getExpressionInDisplay();
 
     if(exprArrayInd.length!==0){
-        exprDisplay.focus();
+        /*exprDisplay.focus();
         const curPos= exprDisplay.selectionStart;
         console.log(curPos);
         const displText= exprDisplay.value; 
         const newText= displText.slice(0,curPos-1)  +displText.slice(curPos); 
-        exprDisplay.value=newText;
-        /*exprDisplay.selectionStart=curPos+1;
-        exprDisplay.selectionEnd=curPos+1; */
+        exprDisplay.value=newText;*/
+      
 
-    /*const lastItemInDisplay=exprArrayInd[exprArrayInd.length-1];
+    const lastItemInDisplay=exprArrayInd[exprArrayInd.length-1];
     //console.log(lastItemInDisplay);
     console.log(lastItemInDisplay.split(""));
     const lastItemDisplayArray=lastItemInDisplay.split("");
@@ -397,7 +396,7 @@ backspaceButton.addEventListener('click', ()=>{
     ];
 
 
-    exprDisplay.value=exprArrayInd.join("");*/
+    exprDisplay.value=exprArrayInd.join("");
 }
     
 
